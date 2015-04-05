89f369319018ef0fce2fb97604fa67f55fadb05b
purmac <acdcpurmac@gmail.com>
2015-04-05 00:27:24 -0400

    First Commit

---
+++ OpenHardwareMonitorQuery.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+
+/* A simple class for making query of Open Hardware Monitor WMI Service */
+namespace PluginOpenHardwareMonitor
+{
+    class OpenHardwareMonitorQuery
+    {
+        public double query(string hwName, string sensorName, string sensorType)
+        {
+            //Need to first specify the Hardware name and the name of the sensor then sensor type
+            //Error checking SensorType or Name is not in specific HardwareName
+            string queryStr;
+            ManagementObject result = new ManagementObject();
+
+            //First get HardwareIdentifier by name
+            string strHwID = getHardwareIdentifierByName(hwName);
+            queryStr = "SELECT value FROM sensor WHERE " + "Parent=\"" + strHwID + "\" AND Name=\"" + sensorName + "\" AND SensorType=\"" + sensorType + "\"";
+            ManagementObjectCollection mCollection = makeQuery(queryStr);
+
+            if (mCollection.Count == 1) //Expecting only one return value
+            {
+                foreach (ManagementObject m in makeQuery(queryStr))
+                {
+                    result = m;
+                    break;
+                }
+                return Convert.ToDouble(result["Value"]);
+            }
+            else
+            {
+                return -1.0;
+            }            
+        }
+
+        private ManagementObjectCollection makeQuery(string queryStr)
+        {
+            ManagementScope scope = new ManagementScope("root\\OpenHardwareMonitor");
+            ObjectQuery query = new ObjectQuery(queryStr);
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+            return searcher.Get();
+        }
+
+        private string getHardwareIdentifierByName(string hwName)
+        {
+            ManagementObject result = null;
+            var queryStr = "SELECT identifier FROM hardware WHERE " + "Name=\"" + hwName + "\"";
+            foreach (ManagementObject mgtObj in makeQuery(queryStr))
+            {
+                result = mgtObj;
+                break;
+            }
+            return result["Identifier"].ToString();
+        }
+
+    }
+}
---
+++ PluginOpenHardwareMonitor.cs
@@ -0,0 +1,130 @@
+﻿// Uncomment these only if you want to export GetString() or ExecuteBang().
+//#define DLLEXPORT_GETSTRING
+//#define DLLEXPORT_EXECUTEBANG
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Rainmeter;
+
+// Overview: This is a blank canvas on which to build your plugin.
+
+// Note: Measure.GetString, Plugin.GetString, Measure.ExecuteBang, and
+// Plugin.ExecuteBang have been commented out. If you need GetString
+// and/or ExecuteBang and you have read what they are used for from the
+// SDK docs, uncomment the function(s). Otherwise leave them commented out
+// (or get rid of them)!
+
+namespace PluginOpenHardwareMonitor
+{
+    internal class Measure
+    {
+        string sensorName, hardwareName, sensorType;
+        internal Measure()
+        {           
+        }
+
+        internal void Reload(Rainmeter.API api, ref double maxValue)
+        {
+            //Read query parameter from Rainmeter
+            sensorName = api.ReadString("SensorName", "");
+            hardwareName = api.ReadString("HardwareName", "");
+            sensorType = api.ReadString("SensorType", "");            
+            //Rainmeter.API.Log(Rainmeter.API.LogType.Debug, sensorType);
+            
+
+
+        }
+
+        internal double Update()
+        {
+            OpenHardwareMonitorQuery q = new OpenHardwareMonitorQuery();
+            double result = q.query(hardwareName, sensorName, sensorType);
+            Rainmeter.API.Log(Rainmeter.API.LogType.Debug, "UPDATE:" + sensorType + " Result:" + result);
+            return result;            
+        }
+        
+#if DLLEXPORT_GETSTRING
+        internal string GetString()
+        {            
+        }
+#endif
+        
+#if DLLEXPORT_EXECUTEBANG
+        internal void ExecuteBang(string args)
+        {
+        }
+#endif
+    }
+
+    public static class Plugin
+    {
+#if DLLEXPORT_GETSTRING
+        static IntPtr StringBuffer = IntPtr.Zero;
+#endif
+
+        [DllExport]
+        public static void Initialize(ref IntPtr data, IntPtr rm)
+        {
+            data = GCHandle.ToIntPtr(GCHandle.Alloc(new Measure()));
+        }
+
+        [DllExport]
+        public static void Finalize(IntPtr data)
+        {
+            GCHandle.FromIntPtr(data).Free();
+            
+#if DLLEXPORT_GETSTRING
+            if (StringBuffer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(StringBuffer);
+                StringBuffer = IntPtr.Zero;
+            }
+#endif
+        }
+
+        [DllExport]
+        public static void Reload(IntPtr data, IntPtr rm, ref double maxValue)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            measure.Reload(new Rainmeter.API(rm), ref maxValue);
+        }
+
+        [DllExport]
+        public static double Update(IntPtr data)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            return measure.Update();
+        }
+        
+#if DLLEXPORT_GETSTRING
+        [DllExport]
+        public static IntPtr GetString(IntPtr data)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            if (StringBuffer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(StringBuffer);
+                StringBuffer = IntPtr.Zero;
+            }
+
+            string stringValue = measure.GetString();
+            if (stringValue != null)
+            {
+                StringBuffer = Marshal.StringToHGlobalUni(stringValue);
+            }
+
+            return StringBuffer;
+        }
+#endif
+
+#if DLLEXPORT_EXECUTEBANG
+        [DllExport]
+        public static void ExecuteBang(IntPtr data, IntPtr args)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            measure.ExecuteBang(Marshal.PtrToStringUni(args));
+        }
+#endif
+    }
+}
